78bf2b17a78e18f3230a58b35698ee73b5d97550
neonbloo25 <theluckdemon@gmail.com>
2025-01-18 16:54:23 -0400

    tweaked lastD and added comment lines

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-if number > 0:
+# 'lastD' trims number variable down to its last digit(preserves negative number status)
+if number >= 0:
     lastD = number % 10
 elif number < 0:
     lastD = number % -10
-
+# This section prints the correct statement of "Last digit of 'xyz' is 'x' etc."
 if lastD <= 5 and lastD != 0:
     print(f"Last digit of {number} is {lastD} and is less than 6 and not 0")
 elif lastD == 0:
