7bbb2bf4a2fa6774295759630b5dd287ce750378
neonbloo25 <theluckdemon@gmail.com>
2025-01-19 22:54:48 -0400

    to 4, bettter explained

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # using an altered version of the former loop
 for i in range(0, 99):
-    # we print from 0 to 98 in hex
+    # we print from 0 to 98, first in regular then in hex
     print("{} = {}".format(i, hex(i)))
