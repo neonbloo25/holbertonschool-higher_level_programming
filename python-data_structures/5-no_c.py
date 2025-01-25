876203c13b5894432fb71dfa6db1d7e966afb2ba
neonbloo25 <theluckdemon@gmail.com>
2025-01-25 01:09:23 -0400

    re re re 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    Cs_and_desist = ['C', 'c']
-    my_string = ''.join([char for char in my_string if char not in Cs_and_desist])
-    return (my_string)
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
