dacf69063d886d9d58160c02bc0edd2a5246a221
neonbloo25 <theluckdemon@gmail.com>
2025-01-26 16:00:19 -0400

    re 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for r in range(x):
         try:
-            print("{}".format(mylist[r]), end="")
+            print("{}".format(my_list[r]), end="")
             i += 1
         except IndexError:
             break
