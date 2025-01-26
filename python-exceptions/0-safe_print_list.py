2e84e38d9e050eb8dd5f8116f9089e00b0330c2e
neonbloo25 <theluckdemon@gmail.com>
2025-01-26 15:50:44 -0400

    to 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for r in range(x):
+        try:
+            print("{}".format(mylist[r]), end="")
+            i += 1
+        except IndexError:
+            break
+    print("")
+    return (i)
