811cc26741570360edfba6be24bf9cd0956dee05
neonbloo25 <theluckdemon@gmail.com>
2025-01-26 22:08:00 -0400

    re-12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
 
     roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     num = 0
+    s1 = "Invalid character '"
+    s2 = "' in Roman numeral string."
 
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+        if roman_string[i] not in roman_dict:
+            raise ValueError("{}{}{}".format(s1, roman_string[i], s2))
 
-        if (i != (len(roman_string) - 1) and
-            roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
+        current_value = roman_dict[roman_string[i]]
+        try:
+            next_value = roman_dict[roman_string[i + 1]]
+        except IndexError:
+            next_value = 0
 
+        if current_value < next_value:
+            num -= current_value
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            num += current_value
+
+    return num
