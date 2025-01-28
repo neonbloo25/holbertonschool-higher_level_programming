a5cd9644c292e3397963178b8613f90578b05f97
neonbloo25 <theluckdemon@gmail.com>
2025-01-28 16:49:13 -0400

    to 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class that contains a private attribute"""
+
+
+class Square():
+    """
+    class Square():
+        This is the class for the current task, a progress from the last one
+
+        Attributes:
+            size: a private attribute intentionally left empty
+
+    pass
+    """
+    def __init__(self, size):
+        self.size = size
+    pass
