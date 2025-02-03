9b87ab4b0206e94f369939860e137cea277bded1
neonbloo25 <theluckdemon@gmail.com>
2025-02-03 07:09:23 -0400

    to ADVANCE!

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Print reverse alphabet using ASCIIs alternating(zYx...A)"""
+
+def tebahpla():
+    result = ""
+    teba1 = 122  # ASCII for 'z'
+    teba2 = 90   # ASCII for 'Z'
+
+    while teba1 > 64:
+        result += chr(teba1)
+        teba1 -= 1
+        if teba1 > 64:
+            result += chr(teba2)
+        teba2 -= 1
+
+    print(result, end="")
+
+tebahpla()
