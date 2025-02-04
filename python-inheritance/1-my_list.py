7ec4f3cae7a1207deb1ad7a47a008ab614c1035d
neonbloo25 <theluckdemon@gmail.com>
2025-02-04 16:11:00 -0400

    to 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class that inherits from a main class"""
+
+
+class MyList(list):
+    """Inheriting Class"""
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        """Target Function"""
+        self.sort(reverse=False)
+
+mylist = MyList()
+mylist.print_sorted()
