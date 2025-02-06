14f83a7ccdf306f902e515d5d933025739efdd11
neonbloo25 <theluckdemon@gmail.com>
2025-02-06 15:14:19 -0400

    to 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a class, empty"""
+
+
+class BaseGeometry:
+    """target class"""
+    def area(self):
+        """target function"""        
+        raise Exception("area() is not implemented")
