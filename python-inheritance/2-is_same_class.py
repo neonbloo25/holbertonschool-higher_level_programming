9f68410ebb9cb529529b9152b6d226751448cbbc
neonbloo25 <theluckdemon@gmail.com>
2025-02-06 12:35:09 -0400

    re 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     """Target Function
     Returns: True/False"""
-    if isinstance(obj, a_class) or type(obj) == type(a_class):
+    if isinstance(obj, a_class) or issubclass(obj, a_class) or type(obj) == type(a_class):
         return True
     else:
         return False
