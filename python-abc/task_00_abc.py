7f601b9dc0dc87df43e007feabc006d14488aa53
neonbloo25 <theluckdemon@gmail.com>
2025-02-07 14:03:33 -0400

    to 00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""The ABCs of Python ft. Abstract Method"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Target Base Class"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """Target Class 1"""
+
+    def sound(self):
+        print("Bark")
+
+
+class Cat(Animal):
+    """Target Class 2"""
+
+    def sound(self):
+        print("Meow")
