850ae7d613509da76c8faa74160be4b52dcf982f
neonbloo25 <theluckdemon@gmail.com>
2025-02-10 00:09:35 -0400

    re 4

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -4,12 +4,27 @@
 
 class Fish:
     """Base Class 1"""
-    pass
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
 
 class Bird:
     """Base Class 2"""
-    pass
+    def fly(self):
+        print("the bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
 
 class FlyingFish(Fish, Bird):
     """Target Class"""
-    pass
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
