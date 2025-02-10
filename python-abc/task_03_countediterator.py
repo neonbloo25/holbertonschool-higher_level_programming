256549fd76ee5e627fe4b4de8ce95eb1412c0736
neonbloo25 <theluckdemon@gmail.com>
2025-02-09 23:54:25 -0400

    re 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -4,4 +4,26 @@
 
 class CountedIterator:
     """Key Class"""
-    pass
+    def __init__(self, data):
+        """func1"""
+        self.data = iter(data)
+        self.counter = 0
+
+    def get_count(self):
+        """
+        func2
+        returns count
+        """
+        return self.counter
+
+    def __next__(self):
+        """
+        func3
+        override __next__
+        """
+        try:
+            item = next(self.data)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration
