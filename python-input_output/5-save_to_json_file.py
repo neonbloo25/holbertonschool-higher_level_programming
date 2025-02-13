cd90fa43cfd7b3fd9ff6da6f6a13a103b6208ca1
neonbloo25 <theluckdemon@gmail.com>
2025-02-12 20:37:09 -0400

    re 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,5 +8,14 @@ def save_to_json_file(my_obj, filename):
     if isinstance(my_obj, set):
         my_obj = list(my_obj)
 
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+    """try/except block running two excepts"""
+    try:
+        """The go-to option"""
+        with open(filename, 'w') as file:
+            json.dump(my_obj, file)
+    except PermissionError as e:
+        """in case of Permissions issue"""
+        print(f"[{e.__class__.__name__}] {e}")
+    except TypeError as e:
+        """in case of type error"""
+        print(f"[{e.__class__.__name__}] {e}")
