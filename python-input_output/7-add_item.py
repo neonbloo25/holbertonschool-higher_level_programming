96b4420ef25d93fbe5e2be8cb766d5a7038f5840
neonbloo25 <theluckdemon@gmail.com>
2025-02-14 15:01:34 -0400

    re re re 7 unexpected

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,11 @@ saveJ = __import__('5-save_to_json_file').save_to_json_file
 loadJ = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item():
-    """Target Function"""
-    filename = "add_item.json"
-    if os.path.exists(filename):
-        items = loadJ(filename)
-    else:
-        items = []
+filename = "add_item.json"
+if os.path.exists(filename):
+    items = loadJ(filename)
+else:
+    items = []
 
-    items.extend(sys.argv[1:])
-    saveJ(filename, items)
+items.extend(sys.argv[1:])
+saveJ(items, filename)
