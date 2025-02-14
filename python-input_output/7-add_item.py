8fa60a925f350e820ecd514bb798cf49da4b2987
neonbloo25 <theluckdemon@gmail.com>
2025-02-14 12:10:38 -0400

    'to 7' massive effin guess

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""I/O Week! tf. Json(ree! ree! ree!)"""
+import json
+saveJ = __import__('5-save_to_json_file').save_to_json_file
+loadJ = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item(filename="", **args):
+    """Target Function"""
+    loadJ(filename)
+    list.append(args)
+    saveJ(filename)
