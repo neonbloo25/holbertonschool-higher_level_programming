237da67f7cf0fc40f7cb429f5a7bc07d76a8b685
neonbloo25 <theluckdemon@gmail.com>
2025-02-14 13:47:26 -0400

    re 7(changed format)

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """I/O Week! tf. Json(ree! ree! ree!)"""
-import json
 import os
+import sys
 saveJ = __import__('5-save_to_json_file').save_to_json_file
 loadJ = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(filename="add_item.json", *args):
+def add_item():
     """Target Function"""
+    filename = "add_item.json"
     if os.path.exists(filename):
         items = loadJ(filename)
     else:
         items = []
 
-    items.extend(args)
+    items.extend(sys.argv[1:])
     saveJ(filename, items)
