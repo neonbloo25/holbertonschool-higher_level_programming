92d1db3223ec99050b49c356ac5585af83f6fcca
neonbloo25 <theluckdemon@gmail.com>
2025-02-15 21:00:20 -0400

    re 8, now with commas

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,36 +6,36 @@ def class_to_json(obj):
     """Target Class"""
     if isinstance(obj, list):
         return {
-            'type': 'list'
-            'length': len(obj)
+            'type': 'list',
+            'length': len(obj),
             'elements': [class_to_json(item) for item in obj]
         }
     elif isinstance(obj, dict):
         return {
-            'type': 'dict'
-            'length': len(obj)
-            'keys': len(obj.keys())
+            'type': 'dict',
+            'length': len(obj),
+            'keys': len(obj.keys()),
             'values': [class_to_json(value) for value in obj.values()]
         }
     elif isinstance(obj, str):
         return {
-            'type': 'string'
-            'length': len(obj)
+            'type': 'string',
+            'length': len(obj),
             'value': obj
         }
     elif isinstance(obj, int):
         return {
-            'type': 'int'
+            'type': 'int',
             'value': obj
         }
     elif isinstance(obj, bool):
         return {
-            'type': 'boolean'
+            'type': 'boolean',
             'value': obj
         }
     else:
         return {
-            'type': 'unknown'
+            'type': 'unknown',
             'value': obj
         }
 
