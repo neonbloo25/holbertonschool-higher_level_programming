a07d76aca816f25e5688f98c49327c5f97a162e8
neonbloo25 <theluckdemon@gmail.com>
2025-02-15 23:25:06 -0400

    to 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,7 +7,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Target Function"""
         return class_to_json(self)
