f09310246ed323715c9b1a23c005f21b7132f400
neonbloo25 <theluckdemon@gmail.com>
2025-02-15 23:24:20 -0400

    to 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""I/O Week! tf. Json(ree! ree! ree!)"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """Target Function"""
+        return class_to_json(self)
+
+
+def class_to_json(obj):
+    """direct insert from last task"""
+    if isinstance(obj, list):
+        return [class_to_json(item) for item in obj]
+
+    elif isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+
+    elif isinstance(obj, str):
+        return obj
+
+    elif isinstance(obj, int):
+        return obj
+
+    elif isinstance(obj, bool):
+        return obj
+
+    elif hasattr(obj, "__dict__"):  # Handling custom class instances
+        return {key: class_to_json(value) for key, value in vars(obj).items()}
+
+    else:
+        """If nothing matches type"""
+        return obj
