58ebcecd9f5df17fe137a5d6c68e9e3f219caa6d
neonbloo25 <theluckdemon@gmail.com>
2025-02-16 23:30:39 -0400

    re 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,8 +4,21 @@
 
 def pascal_triangle(n):
     """Target Function"""
-    tri = []
-    x = 1
-    for r in range(x, n):
-        tri.append[x]
-        print(tri)
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        previous_row = triangle[i - 1]
+        new_row = [1]
+
+        for j in range(len(previous_row) - 1):
+            new_row.append(
+                previous_row[j] + previous_row[j + 1]
+            )
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
