be1441431fbdf72de488ed40a4bc57489f6eb254
neonbloo25 <theluckdemon@gmail.com>
2025-02-23 14:35:29 -0400

    re 3 Upgrade

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,12 +8,28 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     """Class - Request Handler"""
     def do_GET(self):
         """Func - Get requests"""
-        self.send_response(200)
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
 
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
 
-        self.wfile.write(b"Hello, this is a simple API!")
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
 if __name__ == "__main__":
     handler = MyRequestHandler
