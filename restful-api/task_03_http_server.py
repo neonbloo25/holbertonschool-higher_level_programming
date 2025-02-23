0cacef27bb3d9419a61799b217fa177c9a7e3076
neonbloo25 <theluckdemon@gmail.com>
2025-02-23 18:31:40 -0400

    re re re 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b"Ok")
+            self.wfile.write(b"OK")
 
         else:
             self.send_response(404)
