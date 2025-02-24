97d6d8a308b36c46027ae204bdf80b14fa4a92b0
neonbloo25 <theluckdemon@gmail.com>
2025-02-23 22:26:37 -0400

    re 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -24,6 +24,8 @@ def home():
 
 @app.route('/data')
 def data():
+    if not users:
+        return jsonify({"message": "No users found"}), 404
     usernames = list(users.keys())
     return jsonify(usernames)
 
