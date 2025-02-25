cbd70ca63ddf94bb9eaa8aeae46f9a84042135f5
neonbloo25 <theluckdemon@gmail.com>
2025-02-25 15:58:03 -0400

    to 5 take 2

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth
+"""restfulAPI week ft. imports"""
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {}
+
 
 @auth.verify_password
 def verify_password(username, password):
@@ -13,17 +15,25 @@ def verify_password(username, password):
     if user and check_password_hash(user["password"], password):
         return username
 
+
 @app.route('/protected')
 @auth.login_required
 def protected():
-    return jsonify({"message": "This is a protected route", "user": auth.current_user()})
+    return jsonify({
+        "message": "This is a protected route",
+        "user": auth.current_user()
+        })
+
 
 @app.route('/admin')
 @auth.login_required
 def admin():
     if users[auth.current_user()]["role"] != "admin":
-        return jsonify({"message": "You must be an admin to access this route"}), 403
+        return jsonify({
+            "message": "You must be an admin to access this route"
+            }), 403
     return jsonify({"message": "Welcome, admin!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
