278755d8c325765f8757b2adaef8b637e473d094
neonbloo25 <theluckdemon@gmail.com>
2025-03-09 23:48:13 -0400

    to 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
-"""Lists all states in a database"""
-
+""" Python x MySQL : Listing specific data from a database using args """
 import MySQLdb
-import sys
-
-
-def list_states_N(username, password, database_name):
-    """Connects to MySQL server"""
-
-    try:
-        conn = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=username,
-                               password=password,
-                               db=database_name)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM states\
-                       WHERE (name) LIKE BINARY 'N%' ORDER BY states.id ASC;")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error connectig to MySQL or execuiting query: {e}")
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 1-filter_states.py username password database")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        list_states_N(username, password, database_name)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY\
+              'N%' ORDER BY states.id")
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
