2387dc9902f4c49a6b76bc265747db11cf506511
neonbloo25 <theluckdemon@gmail.com>
2025-03-10 15:43:40 -0400

    re 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed as argument from the database"""
+"""prints the State object w/ name passed as argument from db"""
 import sys
 from model_state import Base, State
 from model_city import City
