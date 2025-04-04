ea8d730b581359933fca9c5261e2fd936ed5f309
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 10:20:12 -0400

    assets up

---
+++ javascript-dom_manipulation/script.js
@@ -0,0 +1,3 @@
+let header = document.getElementById("main-header");
+let paragraph = document.getElementsByClassName("description")[0]; // [0] because it returns an array
+let button = document.getElementById("change-btn");
