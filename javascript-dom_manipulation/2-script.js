eaa80c37f699c89d5ff7c6802f38caf2aa193b9a
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 14:32:25 -0400

    re 2

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,3 +1,7 @@
-document.querySelector("#red_header").addEventListener("click", function() {
-  document.querySelector("header").classList.add("red");
+const redHeader = document.getElementById('red_header');
+
+redHeader.addEventListener('click', function() {
+  const header = document.querySelector('header');
+  
+  header.style.color = '#FF0000';
 });
