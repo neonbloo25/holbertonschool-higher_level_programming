d009e353d92f4a1f5b7acb47e036f19ad1dc62a7
neonbloo25 <theluckdemon@gmail.com>
2025-04-04 10:21:00 -0400

    to 3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,8 @@
+document.querySelector("#toggle_header").addEventListener("click", function() {
+  let header = document.querySelector("header");
+  if (header.classList.contains("red")) {
+    header.classList.replace("red", "green");
+  } else {
+    header.classList.replace("green", "red");
+  }
+});
